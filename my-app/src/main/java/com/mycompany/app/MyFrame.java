b65c09e711bd5bff095414a7294b3a9ba20a73d5
Scaramovistk <gabrielscaramaleu@gmail.com>
2024-05-21 15:06:01 +0200

    Testing label modifications

--- my-app/src/main/java/com/mycompany/app/MyFrame.java
+++ my-app/src/main/java/com/mycompany/app/MyFrame.java
@@ -1,8 +1,12 @@
 package com.mycompany.app;
 
+import java.awt.Font;
+
 import javax.swing.*;
-import javafx.scene.paint.Color;
 import javax.swing.JFrame;
+import javax.swing.border.Border;
+
+import java.awt.Color;
 
 public class MyFrame extends JFrame
 {
@@ -11,23 +15,28 @@ public class MyFrame extends JFrame
 		this.setTitle("A RPG Game");
 		this.setSize(600, 600);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setResizable(false);
+		this.setResizable(true);
 
-		JLabel label = new JLabel("Hello World");
-		this.getContentPane().add(label);
-		this.getContentPane().add(label);
-		this.getContentPane().add(label);
-		this.getContentPane().add(label);
+		JLabel label = new JLabel();
+		Border border = BorderFactory.createLineBorder(new Color(0x000000), 2);
 
-		ImageIcon image = new ImageIcon("~/Desktop/cypher Simbol.png");
+		ImageIcon image = new ImageIcon(getClass().getResource("cypher Simbol.png"));
+
+		label.setText("Templar Warrior Simbol");
+		label.setIcon(image);
+		label.setHorizontalTextPosition(JLabel.CENTER);
+		label.setVerticalTextPosition(JLabel.TOP);
+		label.setForeground(new Color(0, 0, 0));
+		label.setFont(new Font("Roboto", Font.ITALIC, 20));
+		label.setBorder(border);
+		label.setVerticalAlignment(JLabel.CENTER);
+		label.setHorizontalAlignment(JLabel.CENTER);
+		label.setBounds(0, 0, 250, 250);
 
 		this.setVisible(true);
+		this.setLayout(null);
+		this.add(label);
+		this.setBackground(new Color(0x0F0F0F));
 		this.setIconImage(image.getImage());
-
-		Color c = new Color(250, 250, 250, 0);
-
-		this.getContentPane().setBackground(null);
-
 	}
-
 }
