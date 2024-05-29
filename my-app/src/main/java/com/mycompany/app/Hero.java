d4cb5e89850262107cf7035d6de54c08b4c886fb
Scaramovistk <gabrielscaramal@gmail.com>
2024-05-29 17:26:05 +0200

    Created here class

---
+++ my-app/src/main/java/com/mycompany/app/Hero.java
@@ -0,0 +1,31 @@
+ublic class Hero
+{
+	String	name;
+	String	class;
+	float	Level;
+	int		Experience;
+	int		Attack;
+	int		Defense
+	int		hitPoints;
+
+	//Attack and defence should be a class ?
+	public Hero()
+	{
+		//Will initialize the argumetns or leave it for the extended class
+	}
+
+	public void Attack()
+	{
+
+	}
+
+	public void Defense()
+	{
+
+	}
+
+	public void updateStatus()
+	{
+		
+	}
+}
