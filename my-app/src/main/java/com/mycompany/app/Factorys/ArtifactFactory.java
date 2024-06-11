284618582ad61615907fcdb98361315d794655a1
Scaramovistk <gabrielscaramaleu@gmail.com>
2024-06-11 15:00:31 +0200

    Created Factorys Classes

---
+++ my-app/src/main/java/com/mycompany/app/Factorys/ArtifactFactory.java
@@ -0,0 +1,20 @@
+
+public class ArtifactFacotory
+{
+	ArtifactFacotory factory;
+
+	private ArtifactFacotory()
+	{
+
+	}
+
+	public ArtifactFacotory getInstance()
+	{
+
+	}
+
+	public Artifact newArtifact(String p_type, int p_quality)
+	{
+
+	}
+}
---
+++ my-app/src/main/java/com/mycompany/app/Factorys/EnemyFactory.java
@@ -0,0 +1,20 @@
+
+public class EnemyFactory
+{
+	EnemyFactory factory;
+
+	private EnemyFactory()
+	{
+
+	}
+
+	public EnemyFacotory getInstance()
+	{
+
+	}
+
+	public Enemy newEnemy(String p_type, int p_level)
+	{
+
+	}
+}
