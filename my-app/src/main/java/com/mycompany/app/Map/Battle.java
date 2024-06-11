d4f645d0cfd989a7ac037006c02fcce5acb7dba1
Scaramovistk <gabrielscaramaleu@gmail.com>
2024-06-11 14:59:11 +0200

    Created Map Class

---
+++ my-app/src/main/java/com/mycompany/app/Map/Battle.java
@@ -0,0 +1,23 @@
+
+public class Battle
+{
+	private Hero hero;
+	private Enemy enemy;
+
+	public Battle(Hero p_hero, Enemy p_enemy)
+	{
+		// Do a check if valid value
+		hero = p_hero;
+		enemy = p_enemy;
+	}
+
+	public voi startBattle()
+	{
+
+	}
+
+	public void showResault()
+	{
+
+	}
+}
---
+++ my-app/src/main/java/com/mycompany/app/Map/Map.java
@@ -0,0 +1,41 @@
+
+public class Map
+{
+	private Hero hero;
+	private int sideLenght;
+
+	public Map()
+	{
+
+	}
+
+	public void generateMap(int p_level)
+	{
+
+	}
+
+	private void populateMap()
+	{
+
+	}
+
+	public void updateConditions()
+	{
+
+	}
+
+	public void startGame()
+	{
+
+	}
+
+	public void createHero()
+	{
+
+	}
+
+	public void finishGame()
+	{
+
+	}
+}
