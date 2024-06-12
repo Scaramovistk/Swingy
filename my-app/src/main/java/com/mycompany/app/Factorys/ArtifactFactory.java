98229ac015bd63787fc7bf7ae4a7b3f319f98dfa
Scaramovistk <gabrielscaramaleu@gmail.com>
2024-06-12 11:44:45 +0200

    Factorys Implemented

--- my-app/src/main/java/com/mycompany/app/Factorys/ArtifactFactory.java
+++ my-app/src/main/java/com/mycompany/app/Factorys/ArtifactFactory.java
@@ -1,20 +1,31 @@
 
-public class ArtifactFacotory
+public class ArtifactFactory
 {
-	ArtifactFacotory factory;
+	private static ArtifactFactory factory;
 
-	private ArtifactFacotory()
+	private ArtifactFactory() {}
+
+	public static ArtifactFactory getInstance()
 	{
-
-	}
-
-	public ArtifactFacotory getInstance()
-	{
-
+		if (factory == null)
+			factory = new ArtifactFactory();
+		return (factory);
 	}
 
 	public Artifact newArtifact(String p_type, int p_quality)
 	{
-
+		if (p_type == null || p_quality < 0)
+			throw new IllegalArgumentException("Invalid argument: Set to null");
+		switch(p_type)
+		{
+			case "Armor":
+				return (new Armor(p_level));
+			case "Helm":
+				return (new Helm(p_level));
+			case "Weapon":
+				return (new Weapon(p_level));
+			default:
+				throw new IllegalArgumentException("Invalid argument: Type font exist");
+		}
 	}
 }
