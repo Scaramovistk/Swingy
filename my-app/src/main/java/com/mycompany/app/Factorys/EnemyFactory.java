a46709ef9052eddae309ce1d3c9c3541688945bb
Scaramovistk <gabrielscaramaleu@gmail.com>
2024-06-12 11:48:24 +0200

    EnemyFactory implemented

---
+++ my-app/src/main/java/com/mycompany/app/Factorys/EnemyFactory.java
@@ -0,0 +1,31 @@
+
+public class EnemyFactory
+{
+	private static EnemyFactory factory;
+
+	private EnemyFactory() {}
+
+	public static EnemyFactory getInstance()
+	{
+		if (factory == null)
+			factory = new EnemyFactory();
+		return (factory);
+	}
+
+	public Enemy newEnemy(String p_type, int p_level)
+	{
+		if (p_type == null || p_level < 0)
+			throw new IllegalArgumentException("Invalid argument: Set to null");
+		switch(p_type)
+		{
+			case "Bandit":
+				return (new Bandit(p_level));
+			case "Dragon":
+				return (new Dragon(p_level));
+			case "Wolf":
+				return (new Wolf(p_level));
+			default:
+				throw new IllegalArgumentException("Invalid argument: Type dont exist");
+		}
+	}
+}
