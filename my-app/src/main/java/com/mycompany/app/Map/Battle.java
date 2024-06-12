2e848e6d0a753f105505cd68b73565f8eb00a8ce
Scaramovistk <gabrielscaramaleu@gmail.com>
2024-06-12 14:34:24 +0200

    Added implementantion plan in Map and Battle

--- my-app/src/main/java/com/mycompany/app/Map/Battle.java
+++ my-app/src/main/java/com/mycompany/app/Map/Battle.java
@@ -7,17 +7,33 @@ public class Battle
 	public Battle(Hero p_hero, Enemy p_enemy)
 	{
 		// Do a check if valid value
+		if (p_hero == null || p_enemy == null)
+			throw Exception();
 		hero = p_hero;
 		enemy = p_enemy;
 	}
 
-	public voi startBattle()
+	public void startBattle()
 	{
+		// make the prompt in a way that can be easly adapted to GUI
+	}
 
+	public void finishBattle()
+	{
+			// If a hero looses a battle, he dies and also looses the mission.
+			// If a hero wins a battle, he gains:
+			// • Experience points, based on the villain power. Of course, he will level up if he
+			// reaches the next level experience.
+			// • An artifact, which he can keep or leave. Of course, winning a battle doesn’t guarantee that an artefact will be droped and the quality of the artefact also varies
+			// depending on the villain’s strenght.
+
+		// Update Hero conditino
+		//If he dies create a new game and finish this one
 	}
 
 	public void showResault()
 	{
-
+		// Show the last player status / Enemy and how much XP he gained
+		//If leveled up show it as well
 	}
 }
--- my-app/src/main/java/com/mycompany/app/Map/Map.java
+++ my-app/src/main/java/com/mycompany/app/Map/Map.java
@@ -1,22 +1,38 @@
 
 public class Map
 {
+	private static Map map;
 	private Hero hero;
 	private int sideLenght;
 
-	public Map()
+	private Map()
 	{
 
 	}
 
+	public static Map getInstance()
+	{
+		if (map == null)
+			map = new EnemyFactory();
+		return (map);
+	}
+
 	public void generateMap(int p_level)
 	{
+		//Block it to just be able to generate one map
 
 	}
 
 	private void populateMap()
 	{
+		createHero();
+		
 
+		//Create the map based in the hero level
+		//Set the map sodeLenght
+
+		//Populate with enemys
+		startGame();
 	}
 
 	public void updateConditions()
@@ -31,7 +47,10 @@ public class Map
 
 	public void createHero()
 	{
-
+		//Pre made heros, create own hero or load it from the txt file
+		if (p_hero == null)
+			throw IllegalArgumentException("Illegal Argument: Hero in the map set to NULL.");
+		hero = p_hero;
 	}
 
 	public void finishGame()
