17da7a8971bc0024f922a1587ebe4f45f0cfc26b
Scaramovistk <gabrielscaramaleu@gmail.com>
2024-06-18 14:38:01 +0200

    Create class Defend

---
+++ my-app/src/main/java/com/mycompany/app/Action/Defend.java
@@ -0,0 +1,11 @@
+package com.mycompany.app.Action;
+
+public class Defend extends Action
+{
+	//Turn in class Action and each action will derivate for this ?
+	public Defend()
+	{
+		super();
+		//which engages him in a battle with the villain
+	}
+}
