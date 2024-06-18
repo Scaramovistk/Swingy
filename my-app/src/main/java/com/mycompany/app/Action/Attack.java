def79d70912cc60daa171b6149d18b57992c3e65
Scaramovistk <gabrielscaramaleu@gmail.com>
2024-06-18 14:37:48 +0200

    Create class Attack

---
+++ my-app/src/main/java/com/mycompany/app/Action/Attack.java
@@ -0,0 +1,12 @@
+package com.mycompany.app.Action;
+
+public class Attack extends Action
+{
+	//Turn in class Action and each action will derivate for this ?
+	public Attack()
+	{
+		super(p_hero, p_enemy);
+		//which engages him in a battle with the villain
+	}
+}
+
